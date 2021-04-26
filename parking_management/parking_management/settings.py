5fa47b7f04127c2a34c5771f2c0d7c7d7c96e0ac
sarath169 <rayala@iiitkalyani.ac.in>
2021-04-26 15:31:51 +0530

    initial commit

--- parking_management/parking_management/settings.py
+++ parking_management/parking_management/settings.py
@@ -9,18 +9,25 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
+import os
 
 from pathlib import Path
 
+import dotenv
+
+dotenv.load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '45n(%m&4zpm-&2ece(()9p8m@hi4*nuuqhx66wl(j4r=m2g)#v'
+SECRET_KEY =
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
